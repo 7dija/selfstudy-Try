7223ca43156f52e6f6040eaa9b3bca3a93221b56
Khadija Salim <7dija9811@gmail.com>
2026-06-15 08:48:20 +0400

    Add files via upload

---
+++ telecom_flow.py
@@ -0,0 +1,6 @@
+import os
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
